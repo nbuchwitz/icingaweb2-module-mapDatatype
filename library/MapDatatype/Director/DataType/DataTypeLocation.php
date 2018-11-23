44f48d01a4e3f081109179a20b28e6f952d09e0d
Gianluca Piccolo <gianluca.piccolo@wuerth-phoenix.com>
2018-11-23 15:55:16 +0100

    Fix Map DataType fields are not populated with inherited values

--- library/MapDatatype/Director/DataType/DataTypeLocation.php
+++ library/MapDatatype/Director/DataType/DataTypeLocation.php
@@ -6,10 +6,6 @@ use Icinga\Module\Director\Hook\DataTypeHook;
 use Icinga\Module\Director\Web\Form\QuickForm;
 use Icinga\Application\Icinga;
 
-//use Zend_Form;
-//use Icinga\Module\Map\Director\Form_Element_Time;
-//use Icinga\Module\Map\Director\ValueFilter\FilterLocation;
-
 class DataTypeLocation extends DataTypeHook
 {
     /**
@@ -17,20 +13,42 @@ class DataTypeLocation extends DataTypeHook
      * @param QuickForm|DirectorObjectForm $form
      *
      * @return \Zend_Form_Element
+     * @throws \Icinga\Exception\ProgrammingError
+     * @throws \Zend_Form_Exception
      */
     public function getFormElement($name, QuickForm $form)
     {
-
+        $placeholder = $this->getPlaceholder($name, $form);
         $module = Icinga::app()
             ->getModuleManager()
             ->loadModule('mapDatatype')
             ->getModule('mapDatatype');
 
         $form->addPrefixPathsForModule($module);
-        //$form->addPrefixPath('Icinga\\Module\\Map\\',)
 
-        $element = $form->createElement('Location', $name);
+        $element = $form->createElement('Location', $name)->setAttribs(array(
+            'placeholder-custom' => $placeholder
+        ));
 
         return $element;
+    }
+
+    private function getPlaceholder($name, $form)
+    {
+        $placeholder = '';
+        $var = $this->adjustVarName($name);
+        if (($value = $form->getObject()->getResolvedVar($var)) !== null) {
+            $placeholder = $value . ' - ' . t('inherited');
+        }
+        return $placeholder;
+    }
+
+    private function adjustVarName($name)
+    {
+        $prefix = 'var_';
+        if (substr($name, 0, strlen($prefix)) == $prefix) {
+            $name = substr($name, strlen($prefix));
+        }
+        return $name;
     }
 }
